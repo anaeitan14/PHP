4e2926797dbdcfdb57b92a7f4a09ee9dc3146d44
Eitan <eitanlich@yahoo.com>
2023-01-01 15:57:16 +0200

    db

---
+++ Database/dbFetch.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Update table info    </title>
+</head>
+
+<body>
+    <?php
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("DATABASE", "db2023");
+
+    $con = mysqli_connect(HOST, USER, PASS, DATABASE);
+    mysqli_set_charset($con, "utf8");
+
+    $sql = "SELECT * FROM users ORDER BY username";
+    $res = mysqli_query($con, $sql);
+
+    echo "<table>
+                <tr>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>Time</th>
+                </tr>";
+
+    while ($row = mysqli_fetch_array($res)) {
+        echo "<tr>
+                <td>$row[1]</td>
+                <td>$row[2]</td>
+                <td>$row[3]</td>
+            </tr>";
+    }
+
+    echo "</table>";
+
+
+
+    mysqli_close($con);
+    ?>
+</body>
+
+</html>
